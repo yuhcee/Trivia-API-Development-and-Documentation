5f90e54fa3dfed9300362c2815f0a006a10201d2
yuhcee <egbouchenna001@gmail.com>
2022-07-19 16:45:49 +0100

    feat(Questions):add an endpoint to DELETE question using a question ID

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -110,6 +110,26 @@ def create_app(test_config=None):
     This removal will persist in the database and when you refresh the page.
     """
 
+    @app.route("/questions/<int:question_id>", methods=["DELETE"])
+    def delete_question_by_id(question_id):
+        try:
+            question = Question.query.filter(Question.id == question_id).one_or_none()
+
+            if question is None:
+                abort(404)
+
+            question.delete()
+
+            return jsonify(
+                {
+                    "success": True,
+                    "deleted": question_id,
+                }
+            )
+
+        except:
+            abort(422)
+
     """
     @TODO:
     Create an endpoint to POST a new question,
