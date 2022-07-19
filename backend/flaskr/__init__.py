3144520f6381eb71dcc643c47fe3a3a9b098c0de
yuhcee <egbouchenna001@gmail.com>
2022-07-19 16:28:52 +0100

    feat(Questions):add endpoint to handle GET requests for questions

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -9,6 +9,30 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
+def fetch_categories():
+    categories_query = Category.query.all()
+
+    if len(categories_query) == 0:
+            abort(404)
+            
+    categories = {}
+        
+    for category in categories_query:
+        categories[category.id] = category.type
+        
+    return categories
+
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
@@ -35,18 +59,11 @@ def create_app(test_config=None):
     """
     @app.route("/categories")
     def retrieve_categories():
-        categories_query = Category.query.all()
-
-        if len(categories_query) == 0:
-            abort(404)
-
-        categories = {}
-        
-        for category in categories_query:
-            categories[category.id] = category.type
+        categories = fetch_categories()
 
         return jsonify(
             {
+                "success" : True,
                 "categories": categories
             }
         )
@@ -63,6 +80,27 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+
+    @app.route("/questions")
+    def retrieve_questions():
+        selection = Question.query.order_by(Question.id).all()
+        current_questions = paginate_questions(request, selection)
+
+        if len(current_questions) == 0:
+            abort(404)
+
+        total_questions = len(Question.query.all())
+        categories = fetch_categories()
+
+        return jsonify(
+            {
+                "success" : True,
+                "questions": current_questions,
+                "totalQuestions": total_questions,
+                "categories": categories,
+                "currentCategory": None
+            }
+        )
 
     """
     @TODO:
